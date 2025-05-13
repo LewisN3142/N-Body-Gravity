bef18d66517ed233234ace56a2422dafbe7ea74e
LewisN3142 <135419079+LewisN3142@users.noreply.github.com>
2025-05-13 11:02:46 +0100

    Refactor (modularisation)
     -- Refactored code to adhere to functional programming principles.
     -- Separated out code for data IO, plotting vectors, acceleration, etc.
     -- Added ability to import test data from file.

--- Gravity.py
+++ Gravity.py
@@ -5,98 +5,157 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-# settings
-isTest = 1;
-testTolerance = 1e-8;
 
-# variables (constants and in/out files)
+##### Version 2 (functional programming) ##################################################################################
+
+#### Settings
+isTest = 1;
+testTolerance = 1e-8; 
+inputFile = 'dataInput';
+outputFile = 'dataOutput';
+arrowScale = 0; # set to 0 for unit vectors
+accelerationMode = 'naive' # Options: 'naive' or 'bh' (Barnes--Hut)
+
+#### (Global) Constants
 gravitationalConstant = 6.7e-11;
 astronomicalUnit = 1.5e11
 solarMass = 2e30;
 
-myPath = os.path.dirname(os.path.abspath(__file__));
+accelerationScale = gravitationalConstant * solarMass / (astronomicalUnit * astronomicalUnit); # Convert units to SI (scales by ~e-3)
 
-if isTest == 0:
-    inputFile = myPath + r'/dataInput.csv';
-else:
-    inputFile = myPath + r'/dataTest.csv' 
+myPath = os.path.dirname(os.path.abspath(__file__)) + r'/';
 
-outputFile = myPath + r'/dataOutput.csv';
 
-# test data (maybe import from file?)
-xAccelerationTest = np.array([1.95682e-6,7.05176e-4,-1.06266e-3]);
-yAccelerationTest = np.array([1.96991e-6,-7.0910e-4,1.05873e-3]);
 
-# merge constants into overall float scale
-accelerationScale = gravitationalConstant * solarMass / (astronomicalUnit * astronomicalUnit);
+#### Functions ############################################################################################################
 
-## import masses and positions from data file and store in arrays (allows for large number of objects)
-df = pd.read_csv(inputFile);
-numberObjects = len(df);
+### Load in data from csv file
+def loadData(chosenInputFile):
+    # Change input file to test version if selected
+    if isTest != 0:
+        chosenInputFile = 'dataTest'
+    
+    # Import data from file (allows for large number of objects)
+    loadPath = myPath + chosenInputFile + '.csv';
+    dataFrame = pd.read_csv(loadPath);
+    print('data imported from ' + loadPath);
+    return dataFrame, chosenInputFile;
+      
+###  Convert dataFrame to dict 
+def dataFrameToNumpyDict(dataFrame):
+    data = dataFrame.to_dict(orient='list');
+    for key in data:
+        data[key] = np.array(data[key]);   
+    return data;
+   
+### Rescale quantities
+def dataRescale(dataDict, scale):
+    for key in dataDict:
+        dataDict[key]*= scale;
+      
+### Plot vectors on graph
+# TODO maybe zip positions, data, acceleration etc into tuples then unzip at end...
+def plotVectors(xPosition, yPosition, xData, yData, labels, vectorScale):
+    fig, ax = plt.subplots();
+    figurePath = myPath + inputFile + r'_' + labels[3] + '.png';
+    
+    if vectorScale == 0:
+        vectorScale = (xData*xData + yData*yData)**(-0.5);
 
-xPositions_List = df.get("xPosition (au)").to_numpy();
-yPositions_List = df.get("yPosition (au)").to_numpy();
-masses_List = df.get("mass (M0)").to_numpy();
-print('data imported');
-
-## compute the accelerations in O(n^2) and store in numpy arrays
-xAcceleration_List = np.zeros(numberObjects);
-yAcceleration_List = np.zeros(numberObjects);
-
-for i in range(1,numberObjects):
-    for j in range(0,i):
-        # note typo in problem
-        changeInX = xPositions_List[j] - xPositions_List[i];
-        changeInY = yPositions_List[j] - yPositions_List[i];
+    for i in range (0,len(xPosition)):
+        ax.quiver(xPosition, yPosition, vectorScale*xData,vectorScale*yData, angles='xy',scale_units='xy',scale=1,color='r');
         
-        # in c++ could implement fast inverse square root for speed
-        radiusCubed = (changeInX * changeInX + changeInY * changeInY)**(-1.5);
-        xAcceleration_List[i] += masses_List[j] * changeInX * radiusCubed;
-        xAcceleration_List[j] -= masses_List[i] * changeInX * radiusCubed;
-        yAcceleration_List[i] += masses_List[j] * changeInY * radiusCubed;
-        yAcceleration_List[j] -= masses_List[i] * changeInY * radiusCubed;  
-print('acceleration computed');
+    plt.grid();
+    plt.title(labels[0]);
+    plt.xlabel(labels[1]);
+    plt.ylabel(labels[2]);
         
-# unit test against initial data (pass if all true)
-if isTest != 0:
-    a = abs(xAcceleration_List - xAccelerationTest) <= testTolerance;
+    fig.gca().set_aspect("equal");
+    fig.savefig(figurePath);
+    plt.close(fig);
+    print('figure saved to ' + figurePath);
+    
+### Unit test against data provided in exercise (pass if all true)
+def testData(dataActual, dataTest):  
+
+    a = abs(dataActual - dataTest) <= testTolerance;
     print(a);
-    b = abs(yAcceleration_List - yAccelerationTest) <= testTolerance;
-    print(b);
-    if (np.prod(a) * np.prod(b) == 0):
+    if (np.prod(a) == 0):
         print("Test failed");
     else:
         print("Test passed");
         
-# rescale acceleration 
-xAccelerationRescaled_List = xAcceleration_List * accelerationScale;
-yAccelerationRescaled_List = yAcceleration_List * accelerationScale;
-
-# output result of calculation to new csv file 
-df['xAcceleration (ms^-2)'] = xAccelerationRescaled_List;
-df['yAcceleration (ms^-2)'] = yAccelerationRescaled_List;
-
-df.index += 1;
-df.to_csv(outputFile, encoding='utf-8', index=True, header=True);
-print('data saved');
-
-
-# print plot of positions and directions/relative magnitudes of acceleration 
-fig, ax = plt.subplots() 
-
-accelerationUnit = 5000; # switch to normalised... (make setting)
-
-for i in range(0,numberObjects):
-    ax.quiver(xPositions_List[i],yPositions_List[i],accelerationUnit * xAcceleration_List[i],accelerationUnit * yAcceleration_List[i],angles='xy',scale_units='xy',scale=1, color='r');
+### Output data to csv file
+def saveData(dataFrame, chosenOutputFile):
+    savePath = myPath + chosenOutputFile + '.csv';
+    df.to_csv(savePath, encoding='utf-8', index=True, header=True);
+    print('data saved to ' + savePath);
     
-plt.grid();
-plt.title('Acceleration vectors of particles in ' + str(numberObjects) + '-body system');
-plt.xlabel('x Position (au)');
-plt.ylabel('y Position (au)');
-fig.gca().set_aspect("equal");
-fig.savefig(myPath + '/figure 1.png');
-plt.show();
-plt.close(fig);
-print('figure saved');
+   
+   
+### Acceleration Code #####################################################################################################
 
+## Naive algorithm for computing acceleration with O(n^2) complexity
+
+# Compute acceleration (in non SI units)
+def naiveAcceleration(xPosition, yPosition, masses): 
+    
+    numberObjects = len(masses)
+    xAcceleration = np.zeros(numberObjects);
+    yAcceleration = np.zeros(numberObjects);
+    
+    for i in range(1,numberObjects):
+        for j in range(0,i):
+            # note typo in problem, can we do this using numpy operations to improve efficiency...
+            changeInX = xPosition[j] - xPosition[i];
+            changeInY = yPosition[j] - yPosition[i];
+            radiusCubed = (changeInX * changeInX + changeInY * changeInY)**(-1.5); # could use fast inverse sqrt in c++
+            
+            xAcceleration[i] += masses[j] * changeInX * radiusCubed;
+            xAcceleration[j] -= masses[i] * changeInX * radiusCubed;
+            yAcceleration[i] += masses[j] * changeInY * radiusCubed;
+            yAcceleration[j] -= masses[i] * changeInY * radiusCubed;  
+      
+    print('acceleration computed');
+    return {"xAcceleration (ms^-2)": xAcceleration, "yAcceleration (ms^-2)":yAcceleration};
+  
+
+## Barnes Hut Algorithm
+# TODO add code
+def barnesHutAcceleration(xPosition, yPosition, masses):
+    
+
+
+
+# Wrapper function (compute, test, plot, rescale)
+def accelerationWrapper(data, mode):
+    
+    if mode == 'naive':
+        acceleration = naiveAcceleration(data["xPosition (au)"],data["yPosition (au)"],data["mass (M0)"]);
+    elif mode == 'bh':
+        acceleration = barnesHutAcceleration(data["xPosition (au)"],data["yPosition (au)"],data["mass (M0)"]);
+
+    if isTest != 0:
+        testData(np.concatenate((acceleration["xAcceleration (ms^-2)"], acceleration["yAcceleration (ms^-2)"])),np.concatenate((data["xAccelerationTest"], data["yAccelerationTest"])));
+        
+    accelerationFigureLabels = ['Acceleration vectors of particles in ' + str(len(data["xPosition (au)"])) + '-body system', 'x Position (au)', 'y Position (au)', 'acceleration']
+    
+    plotVectors(data["xPosition (au)"],data["yPosition (au)"],acceleration["xAcceleration (ms^-2)"], acceleration["yAcceleration (ms^-2)"],accelerationFigureLabels,arrowScale);
+    
+    dataRescale(acceleration, accelerationScale);
+    return acceleration;
+
+  
+#### Execution
+
+# load data, clean, then convert to numpy
+df, inputFile = loadData(inputFile); 
+df = dataFrameToNumpyDict(df);
+
+# Perform computations
+df.update(accelerationWrapper(df,accelerationMode));
+
+# Convert back to dataFrame and export
+df = pd.DataFrame.from_dict(df);
+saveData(df, outputFile);
 
